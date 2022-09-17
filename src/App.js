7b21035c9143ab8a82bfe6e13e1f7674eaa00692
AprilianoPeramu <gaminggorse@gmail.com>
2022-09-17 23:17:52 +0800

    remake logic login and add toast for pop up

--- src/App.js
+++ src/App.js
@@ -7,21 +7,34 @@ import Login from './pages/Login';
 import Cart from './pages/Cart';
 import Contact from './pages/Contact';
 import Register from "./pages/Register";
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <>      
+    <>
       <Navbar />
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/product' element={<Product/>} />
-        <Route path='/contact' element={<Contact/>} />
-        <Route path='/cart' element={<Cart/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/product' element={<Product />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/cart' element={<Cart />} />
         <Route path='/Login' element={<Login />} />
         <Route path='/Register' element={<Register />} />
       </Routes>
+      <ToastContainer />
     </>
 
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,135 +1,130 @@
-import React, { Component } from 'react';
-import { Link, } from 'react-router-dom';
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import {toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+export const Login = () => {
 
-class Login extends Component {
-        state = {
-            field: {
-                email: '',
-                password: ''
-            },
-            errors: {
-                email: '',
-                password: ''
+    const [email, setEmail] = useState('');
+    const [emailError, setEmailError] = useState('');
+
+    const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+
+    const [passwordRemember, setPasswordRemember] = useState('');
+    
+    const navigate = useNavigate();
+
+    const handleEmailChange=(e)=>{
+       
+        setEmailError('');
+        setEmail(e.target.value);
+    }
+
+    const handlePasswordChange=(e)=>{
+        
+        setPasswordError('');
+        setPassword(e.target.value);
+    }
+
+    const handlePassworRememberdChange=(e)=>{
+
+        setPasswordRemember(e.target.value);
+    }
+
+    const handleFormSubmit=(e)=>{
+        e.preventDefault();
+        
+        if(email!==''){
+            const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+            if(emailRegex.test(email)){
+                toast.success('Welcome To Our Market', {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    });
+
+                    navigate('/')
+                    
+                localStorage.setItem('email', email)
+                localStorage.setItem('password', password)
+            }
+            else{
+                setEmailError('Invalid Email')
             }
         }
-
-        handleOnValidate = (name, value) => {
-            if (!value) {
-                return ' Please input field'
-            }
-
-            if (name === 'email' && !value.includes('@')) {
-                return 'Email is not valid'
-            }
-
-            if (name === 'password' && value.length < 8) {
-                return 'Password is too short'
-            }
-
-            return false
+        else{
+            setEmailError('Email is Required');
         }
 
-        handleOnChange = (event) => {
-            const { field, errors } = this.state
-            const { value, name } = event.target
-            const error = this.handleOnValidate(name, value)
-            this.setState({
-                field: {
-                    ...field,
-                    [name]: value
-                },
-                errors: {
-                    ...errors,
-                    [name]: error
-                }
-            })
+        if(password!==''){
 
         }
-
-        handleFormOnSubmitValidation = () => {
-            const { field } = this.state
-            const names = Object.keys(field)
-            const errors = {}
-            names.forEach((name) => {
-                const value = field[name]
-                const error = this.handleOnValidate(name, value)
-                errors[name] = error
-            })
-
-            return errors
-        }
-
-        handleFormOnSubmit = (event) => {
-            event.preventDefault()
-            const validation = this.handleFormOnSubmitValidation()
-            const hasError = Object.values(validation).some((item) => item !== false)
-            if ( hasError ) {
-                this.setState({ errors: validation })
-                return 
-            }
-            const { field } = this.state
-            console.log(validation, field)
-        }
-        render() {
-            const { field, errors } = this.state
-            return (
-                <div>
-                    <form onSubmit={this.handleFormOnSubmit} >
-                        <section className="vh-100 bg-success">
-                            <div className="container py-5 h-100">
-                                <div className="row d-flex justify-content-center align-items-center h-100">
-                                    <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                                        <div className="card shadow-2-strong" style={{ borderRadius: 16 }}>
-                                            <div className="card-body p-5 text-center">
-
-                                                <h3 className="mb-5">SIGN IN</h3>
-
-                                                <div className="form-outline mb-4">
-                                                    <input type="email" id="typeEmailX-2" name="email" className="form-control form-control-lg"
-                                                        value={field.email}
-                                                        onChange={this.handleOnChange}
-                                                    />
-                                                    <p>{errors.email}</p>
-                                                    <label className="form-label" for="typeEmailX-2 ">Email</label>
-                                                </div>
-
-                                                <div className="form-outline mb-4">
-                                                    <input type="password" id="typePasswordX-2" name="password" className="form-control form-control-lg"
-                                                        value={field.password}
-                                                        onChange={this.handleOnChange}
-                                                    />
-                                                    <p>{errors.password}</p>
-                                                    <label className="form-label" for="typePasswordX-2">Password</label>
-                                                </div>
-
-                                                {/* Checkbox */}
-                                                <div className="form-check d-flex justify-content-start mb-4">
-                                                    <input className="form-check-input" type="checkbox" value="" id="form1Example3" />
-                                                    <label className="form-check-label" for="form1Example3"> Remember password </label>
-                                                </div>
-
-                                                <button className="btn btn-primary btn-lg btn-block" type="submit">LOGIN</button>
-
-                                                <hr className="my-4" />
-
-                                                <p>Don't have an account?</p>
-                                                <Link to="/Register">Register here</Link>
-
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
-                    </form>
-                </div>
-
-
-            );
-            
+        else{
+            setPasswordError('Password Required');
         }
     }
 
+  return (
+    <div>
+        <form onSubmit={handleFormSubmit} >
+                <section className="vh-100 bg-success">
+                    <div className="container py-5 h-100">
+                        <div className="row d-flex justify-content-center align-items-center h-100">
+                            <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+                                <div className="card shadow-2-strong" style={{ borderRadius: 16 }}>
+                                    <div className="card-body p-5 text-center">
+
+                                        <h3 className="mb-5">SIGN IN</h3>
+
+
+                                        <div className="form-outline mb-4">
+                                            <input type="email" id="typeEmailX-2" name="email" className="form-control form-control-lg"
+                                                value={email}
+                                                onChange={handleEmailChange}
+                                            />
+                                            {emailError&&<p style={{ color : "red"}}>{emailError}</p>}
+                                            <label className="form-label" for="typeEmailX-2 ">Email</label>
+                                        </div>
+
+                                        <div className="form-outline mb-4">
+                                            <input type="password" id="typePasswordX-2" name="password" className="form-control form-control-lg"
+                                                value={password}
+                                                onChange={handlePasswordChange}
+                                            />
+                                            {passwordError&&<p style={{ color : "red"}}>{passwordError}</p>}
+                                            <label className="form-label" for="typePasswordX-2">Password</label>
+                                        </div>
+
+                                        {/* Checkbox */}
+                                        <div className="form-check d-flex justify-content-start mb-4">
+                                            <input className="form-check-input" type="checkbox" id="form1Example3" 
+                                            value={passwordRemember}
+                                            onChange={handlePassworRememberdChange}
+                                            />
+                                            <label className="form-check-label" for="form1Example3"> Remember password </label>
+                                        </div>
+
+                                        <button className="btn btn-primary btn-lg btn-block" type="submit" >LOGIN</button>
+
+                                        <hr className="my-4" />
+
+                                        <p>Don't have an account?</p>
+                                        <Link to="/Register">Register here</Link>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </form>
+    </div>
+  )
+}
 
 export default Login;
