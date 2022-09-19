fe4b076c639bb3245729067eea1888ed40a9b295
AprilianoPeramu <gaminggorse@gmail.com>
2022-09-19 12:58:33 +0800

    make a logic for register and deleted a few fiture

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,78 +1,133 @@
-import React, { Component} from 'react';
-import { Link, } from 'react-router-dom';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import {toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+export const Register = () => {
 
-class Register extends Component {
-    state = {
-        username: '',
-        email: '',
-        password: ''
+    const [username, setUsername] = useState('');
+    const [usernameErr, setUsernameErr] = useState('');
+
+    const [email, setEmail] = useState('');
+    const [emailError, setEmailError] = useState('');
+
+    const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    
+    const navigate = useNavigate();
+
+    const handleUsernameChange = (e) => {
+        setUsernameErr('');
+        setUsername(e.target.value);
     }
 
-    handleOnChange = (event) => {
-        const { value, name } = event.target
-        this.setState({ [name]: value })
+    const handleEmailChange=(e)=>{
+       
+        setEmailError('');
+        setEmail(e.target.value);
     }
-    handleFormOnSubmit = (event) => {
-        event.preventDefault()
-        const {email, password} = this.state
-        localStorage.setItem('form', JSON.stringify(this.state))
-        console.log({email, password})
+
+    const handlePasswordChange=(e)=>{
+        
+        setPasswordError('');
+        setPassword(e.target.value);
     }
-    render () {
-    return (
-        <div>
-            <form onSubmit={this.handleFormOnSubmit}>
-            <section className="vh-100 bg-success">
-                <div className="container py-5 h-100">
-                    <div className="row d-flex justify-content-center align-items-center h-100">
-                        <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                            <div className="card shadow-2-strong" style={{ borderRadius : 16}}>
-                                <div className="card-body p-4 text-center">
 
-                                    <h3 className="mb-5">SIGN UP</h3>
+    const handleFormSubmit=(e)=>{
+        e.preventDefault();
+        
+        if(email!==''){
+            const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+            if(emailRegex.test(email)){
+                toast.success('You Have Successfully Registered', {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    });
 
-                                    <div className="form-outline mb-4">
-                                        <input type="username" id="typeUserNameX-2" name='username' className="form-control form-control-lg" required='Input Name' placeholder='Username'
-                                        value={this.state.username}
-                                        onChange={this.handleOnChange}
-                                        />
-                                        <label className="form-label" for="typeUserNameX-2">UserName</label>
+                    navigate('/login')
+                    
+                localStorage.setItem('email', email)
+                localStorage.setItem('password', password)
+            }
+            else{
+                setEmailError('Invalid Email')
+            }
+        }
+        else{
+            setEmailError('Email is Required');
+        }
+
+        if(password!==''){
+
+        }
+        else{
+            setPasswordError('Password Required');
+        }
+
+        if(username!==''){
+
+        }
+        else{
+            setUsernameErr('Username Required')
+        }
+    }
+
+  return (
+    <div>
+        <form onSubmit={handleFormSubmit} >
+                <section className="vh-100 bg-success">
+                    <div className="container py-5 h-100">
+                        <div className="row d-flex justify-content-center align-items-center h-100">
+                            <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+                                <div className="card shadow-2-strong" style={{ borderRadius: 16 }}>
+                                    <div className="card-body p-4 text-center ">
+
+                                        <h3 className="mb-5">SIGN UP</h3>
+
+                                        <div className="form-outline mb-4">
+                                            <input type="username" id="typeUsernameX-2" name="username" className="form-control form-control-lg"
+                                                value={username}
+                                                onChange={handleUsernameChange}
+                                            />
+                                            {usernameErr&&<p style={{ color : "red"}}>{usernameErr}</p>}
+                                            <label className="form-label" for="typeEmailX-2 ">Username</label>
+                                        </div>
+
+
+                                        <div className="form-outline mb-4">
+                                            <input type="email" id="typeEmailX-2" name="email" className="form-control form-control-lg"
+                                                value={email}
+                                                onChange={handleEmailChange}
+                                            />
+                                            {emailError&&<p style={{ color : "red"}}>{emailError}</p>}
+                                            <label className="form-label" for="typeEmailX-2 ">Email</label>
+                                        </div>
+
+                                        <div className="form-outline mb-4">
+                                            <input type="password" id="typePasswordX-2" name="password" className="form-control form-control-lg"
+                                                value={password}
+                                                onChange={handlePasswordChange}
+                                            />
+                                            {passwordError&&<p style={{ color : "red"}}>{passwordError}</p>}
+                                            <label className="form-label" for="typePasswordX-2">Password</label>
+                                        </div>
+
+                                        <button className="btn btn-primary btn-lg btn-block" type="submit" >LOGIN</button>
+
                                     </div>
-
-                                    <div className="form-outline mb-4">
-                                        <input type="email" id="typeEmailX-2" name='email' className="form-control form-control-lg" required='Input Name' placeholder='example@gmail.com'
-                                        value={this.state.email}
-                                        onChange={this.handleOnChange}
-                                        />
-                                        <label className="form-label" for="typeEmailX-2">Email</label>
-                                    </div>
-
-                                    <div className="form-outline mb-4">
-                                        <input type="password" id="typePasswordX-2" name='password' className="form-control form-control-lg" required='Input Name' placeholder='Password'
-                                        value={this.state.password}
-                                        onChange={this.handleOnChange}
-                                        />
-                                        <label className="form-label" for="typePasswordX-2">Password</label>
-                                    </div>
-
-                                    <button className="btn btn-primary btn-lg btn-block" type="submit">SIGN UP</button>
-
-                                    <hr className="my-4" />
-
-                                    <p>Allready have an account?</p>
-                                    <Link to="/Register">Register here</Link>
-
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
             </form>
-        </div>
-    );
-    }
+    </div>
+  )
 }
 
 export default Register;
