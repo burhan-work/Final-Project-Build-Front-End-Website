1a09180a06d783711f8979496d74d286d2c63701
Storynaindah <indahstorynaheriati@gmail.com>
2022-09-19 22:35:06 +0700

    products

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
-import Navbar from "./components/Navbar";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Product from "./pages/Product";
 // import Login from './pages/Login';
-import Login from './pages/Login';
-import Cart from './pages/Cart';
-import Contact from './pages/Contact';
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Contact from "./pages/Contact";
 import Register from "./pages/Register";
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>      
+    <>
       <Navbar />
       {/* <Login />  */}
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/product' element={<Product/>} />
-        <Route path='/contact' element={<Contact/>} />
-        <Route path='/cart' element={<Cart/>} />
-        <Route path='/Login' element={<Login />} />
-        <Route path='/Register' element={<Register />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
       </Routes>
     </>
-    
-
   );
 }
 
