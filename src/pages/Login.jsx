f824afc270bf86bf40f4ed2897ebef51b2b2c99e
AprilianoPeramu <gaminggorse@gmail.com>
2022-09-20 13:00:08 +0800

    change bg color

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -65,7 +65,7 @@ export const Login = () => {
   return (
     <div>
         <form onSubmit={handleFormSubmit} >
-                <section className="vh-100 bg-success ">
+                <section className="vh-100 bg-secondary ">
                     <div className="container py-5 h-100">
                         <div className="row d-flex justify-content-center align-items-center h-100">
                             <div className="col-12 col-md-8 col-lg-6 col-xl-5">
