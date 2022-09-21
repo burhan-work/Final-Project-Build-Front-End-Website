f1ed126640bac60b1a9965bcca8ddf2982da3d4f
AprilianoPeramu <gaminggorse@gmail.com>
2022-09-21 14:48:03 +0800

    fix home and add button go to product

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import Product from "./Product";
 
 const Home = () => {
+    const navigate = useNavigate('');
+
+    const handleOnClick = (e) => {
+        e.preventDefault();
+        navigate('/Product')
+    }
+
     return (
         <div className='hero'>
-            <div className="card text-bg-dark">
-                <img src="/assets/bg.jpeg" className="card-img" alt="Backgorund"/>
+            <div className="card text-bg-dark text-white border-0 ">
+                <img src="/assets/bg.jpeg" className="card-img" alt="Backgorund" height="600px"/>
                     <div className="card-img-overlay d-flex flex-column justify-content-center">
                         <div className="container">
                         <h5 className="card-title display-3 fw-bolder mb-0" style={{color: "black"}}>NEW SEASON ARRIVALS</h5>
                         <p className="card-text lead fs-2" style={{color: "black"}}>
                             CHECK OUT ALL THE TRENDS
                         </p>
-                        <p className="card-text"><small>Last updated 3 mins ago</small></p>
+                        <button type="button" class="btn btn-secondary" onClick={handleOnClick}>Check Now</button>
                         </div>
                     </div>
             </div>
