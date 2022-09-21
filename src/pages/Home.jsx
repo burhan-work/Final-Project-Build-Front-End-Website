bfc7d771f8899eac0951b9be0d6164e11b1c902b
AprilianoPeramu <gaminggorse@gmail.com>
2022-09-21 14:10:04 +0800

    basic home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from 'react'
+import Product from "./Product";
 
-function Home(props) {
+const Home = () => {
     return (
-        <div>
-            Rumahku
+        <div className='hero'>
+            <div className="card text-bg-dark">
+                <img src="/assets/bg.jpeg" className="card-img" alt="Backgorund"/>
+                    <div className="card-img-overlay d-flex flex-column justify-content-center">
+                        <div className="container">
+                        <h5 className="card-title display-3 fw-bolder mb-0" style={{color: "black"}}>NEW SEASON ARRIVALS</h5>
+                        <p className="card-text lead fs-2" style={{color: "black"}}>
+                            CHECK OUT ALL THE TRENDS
+                        </p>
+                        <p className="card-text"><small>Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+            </div>
+            <Product />
         </div>
-    );
+    )
 }
 
 export default Home;
