5377fe60a205e0729069cda1db5731f33f8ad512
FebbyLimasSendelvant <febbylimas98@gmail.com>
2022-09-21 13:57:47 +0800

    contact

--- src/App.js
+++ src/App.js
@@ -9,41 +9,47 @@ import Login from './pages/Login';
 import Cart from './pages/Cart';
 import Contact from './pages/Contact';
 import Register from "./pages/Register";
+import Footer from "./pages/Footer";
 import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <ToastContainer
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
 
-      {/* <Login />  */}
+      <>
+        <Navbar />
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
 
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/product' element={<Product />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/Login' element={<Login />} />
-        <Route path='/Register' element={<Register />} />
-      </Routes>
-      <ToastContainer />
-    </>
-    
+        {/* <Login />  */}
+        
 
-  );
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/product' element={<Product />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/Login' element={<Login />} />
+          <Route path='/Register' element={<Register />} />
+          <Route path='/Footer' element={<Footer />} />
+        </Routes>
+        <ToastContainer />
+        
+      </>
+      
+
+
+      );
 }
 
-export default App;
+      export default App;
